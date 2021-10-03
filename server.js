511847dd25aa89a9fdb925c42a36a9645449ffac
Ibifuro Odu <oduibifuro@gmail.com>
2021-10-02 20:46:51 -0400

    Re-arranged file heirachy

--- server.js
+++ server.js
@@ -9,54 +9,63 @@ const PORT = 3000;
 const chatbot = "Chabot";
 
 //get external modules
-const { formatMessage} = require("./FE/formatMessage.js");
-const { formatUsers, userList} = require("./FE/users.js");
+const {botMessage} = require("./FE/formatMessage.js");
+const {formatUsers,userList,searchArrayObj} = require("./FE/users.js");
 
 app.use(express.static(path.join(__dirname + "/FE/")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-let userData = [];
+let userData =[];
+let roomData = ["travel & tourism","shopping","expat","fishing"];
+let memberCount = 0;
 
 io.on("connection", (socket) => {
   
   const userId = socket.id; 
-
   //recieves the first message from the client
   socket.on("userJoin", (userDetails) => {
-    let user = userList(userId, userDetails.username, userDetails.room);
-    userData.push(user);
-    io.emit("onlineUsers", userData);
-    io.emit("newUser", user);
+
+    
+      socket.join(userDetails.chosenRoom);
+      //add create a complete user object
+      let user = userList(userId, userDetails.username, userDetails.chosenRoom);
+      userData.push(user);
+      //send to client 
+      io.emit("onlineUsers", userData);
+      io.emit("newUser", user);
+      
+      //welcome user to room
+      io.to(user.room).emit(
+        'broadcastMessage',botMessage(chatbot,`Welcome to the ${user.room} club ${user.username}!`));
+    
   });
 
-  socket.emit(
-    "BroadcastMessage",
-    formatMessage(chatbot, "Welcome to your first message!")
-  );
-
-  socket.emit(
-    "BroadcastMessage",
-    formatMessage(chatbot, "A user has joined the chat!")
-  );
-
-  socket.on("disconnect", () => {
-  //removes disconnected users from the array 
-    let leftUser = userData.indexOf(userId);
-    if (leftUser >= 0) userData.splice(leftUser, 1);
-    io.emit("removeUser", userId);
-    io.emit(
-      "BroadcastMessage",
-      formatMessage(chatbot, "A user has left the chat")
-    );
+  socket.on("chatMessage", (msg) => {
+    io.in(msg.chosenRoom).emit("newMessage", formatUsers(msg, userId));
   });
+
+  socket.on("privateMessage", (msg) => {
+   socket.join(msg.recipientId);
+   io.in(msg.recipientId).emit("incomingPrivateMessage",formatUsers(msg));
+  });
+
+  io.emit("AvailableRooms",(roomData));
 
   socket.on("typing", (username) => {
     io.emit("incoming", username);
   });
-
-  socket.on("chat_message", (msg) => {
-    io.emit("NewMessage", formatUsers(msg, userId));
-  });
+ 
+  socket.on("disconnect", () => {
+    //removes disconnected users from the array 
+      let location = searchArrayObj(userId,userData);
+      let leftUser = userData.indexOf(location);
+        if (leftUser >= 0){userData.splice(leftUser, 1);}
+          io.emit("removeUser", userId);
+            socket.emit(
+              "BroadcastMessage",
+              botMessage(chatbot, "A user has left the chat")
+            );
+    });
 });
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
